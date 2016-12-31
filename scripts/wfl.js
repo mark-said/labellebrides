49f4bbfb765345f85f98301ce5c2c0d02c750499
Jim Ramsden <jim@etchapps.com>
2016-12-31 19:46:16 +0000

    Migrating site

---
+++ scripts/wfl.js
@@ -0,0 +1,22 @@
+//
+// Web Font Loader
+//
+
+  WebFontConfig = {
+    google: { 
+      families: [ 
+      'Josefin+Sans:300,600,300italic,600italic:latin',
+      'Monsieur+La+Doulaise::latin' 
+      ] 
+    }
+  };
+
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
